2e188f47469579c198407d70e38f13d67e413da4
Vyacheslav <51016007+Ellinist@users.noreply.github.com>
2020-08-02 17:09:41 +0300

    Add files via upload

---
+++ DocsVisionClasses/Attachment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocsVisionClient.DocsVisionClasses
+{
+    /// <summary>
+    /// Класс вложений писем
+    /// </summary>
+    class Attachments
+    {
+        public int IDAttachment { get; set; }
+        public int IDLetterAttachment { get; set; }
+        public byte[] Attachment { get; set; }
+    }
+}
